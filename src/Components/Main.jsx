fdfa0f7b38b6ce254d0ca1ffefcadfa136120215
baryshkov <ambaryshkov@gmail.com>
2020-11-05 11:46:20 +0300

    delete unnecessary console.log

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -25,7 +25,6 @@ const MIN_MAX = 999999;
 // 2/11/20 22:30 - 23:17
 // 4/11/20 15:51 - 16:40
 const Main = ({ fetchedUser }) => {
-  console.log(11111);
   const quizService = new QuizService(NUMBER_OF_OPTIONS, MIN_MAX);
 
   const [score, setScore] = useState(0);
